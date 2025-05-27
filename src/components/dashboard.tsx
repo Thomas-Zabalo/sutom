9e2700706518eecbf121b76fe9963f038cdc9acf
tzabalo <t.zabalo@cdg82.fr>
2025-05-27 16:55:55 +0200

    refacto(style): remove dayword value in dashboard pass with props now

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,24 +1,22 @@
-import {useState} from "react";
-
 interface value {
     keyChar: string;
+    dayWord: string;
 }
 
-export default function Dashboard({keyChar}: value) {
-    const [dayWord] = useState<string>('Jeanot');
+export default function Dashboard({keyChar, dayWord}: value) {
     const keyIndex = keyChar.toUpperCase().split('');
 
     const rows = [];
 
-    for (let i = 0; i < 7; i++) {
-        rows.push(
-            <tr className='text-center'>
-                <th>{dayWord[0]}</th>
-                {dayWord.split('').slice(1).map((_, index) => (
-                    <th key={index + 1}>{keyIndex[index] ? keyIndex[index] : '.'}</th>
-                ))}
-            </tr>)
-    }
+
+    rows.push(
+        <tr className='text-center'>
+            <th>{dayWord[0]}</th>
+            {dayWord.split('').slice(1).map((_, index) => (
+                <th key={index + 1}>{keyIndex[index] ? keyIndex[index] : '_'}</th>
+            ))}
+        </tr>)
+
 
     return (
         <>
